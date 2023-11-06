ca57dffa9a347f84d2868130652a41c4afc933b8
Alex McIntire <imalexmcintire@gmail.com>
2023-11-05 21:32:54 -0500

    (feat): created form

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ 
+    extended: true 
+}));
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     //res.send('Hello World!');
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
